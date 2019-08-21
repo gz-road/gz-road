4d3d2afdfe44bf7c37b8105be23a5deb7129a5e2
Luoxiating <2239789228@qq.com>
2019-08-21 11:39:15 +0800

    文件更新

---
+++ road/js/map.js
@@ -0,0 +1,7 @@
+var map = new AMap.Map('map1', {
+    resizeEnable: true,
+    zoom:10,
+    center: [106.63024, 26.64702],
+    //前往创建自定义地图样式：https://lbs.amap.com/dev/mapstyle/index
+});
+map.setMapStyle('amap://styles/76f0c018533adc82f3250635bfb06e24');
