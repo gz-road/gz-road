2b9f9c61c91a77c6665a9acf90a444ee4c7de58d
Luoxiating <2239789228@qq.com>
2019-08-23 13:57:47 +0800

    文件整合

--- road/js/roadassets.js
+++ road/js/roadassets.js
@@ -57,12 +57,65 @@ for (var i = 0; i < scaleData.length; i++) {
         itemStyle: placeHolderStyle
     });
 }
+var plantCap = [{
+    name: '路面',
+    value: '150',
+
+}, {
+    name: '隧道',
+    value: '115',
+
+},{
+    name: '桥梁',
+    value: '50',
+
+},]
+
+var datalist = [{
+    offset: [64, 55],
+    symbolSize: 50,
+    opacity: .95,
+    fontSize: '14'
+}, {
+    offset: [34, 63],
+    symbolSize: 36,
+    opacity: .95,
+    fontSize: '12'
+}, {
+    offset: [44, 20],
+    symbolSize: 40,
+    opacity: .95,
+    fontSize: '12'
+}]
+var datas = [];
+for (var i = 0; i < plantCap.length; i++) {
+    var item = plantCap[i];
+    var itemToStyle = datalist[i];
+    datas.push({
+        name: item.name,
+        value: itemToStyle.offset,
+        symbolSize: itemToStyle.symbolSize,
+        label: {
+            normal: {
+                textStyle: {
+                    fontSize: itemToStyle.fontSize
+                }
+            },
+
+        },
+        itemStyle: {
+            normal: {
+                opacity: itemToStyle.opacity
+            }
+        },
+    })
+}
 var seriesObj = [{
     name: '',
     type: 'pie',
     clockWise: false,
     radius: ['56%', '74%'],
-    hoverAnimation: false,
+    hoverAnimation: true,
     itemStyle: {
         normal: {
             label: {
@@ -78,18 +131,65 @@ var seriesObj = [{
         }
     },
     data: data
-}];
+},
+    {
+        type: 'scatter',
+        symbol: 'circle',
+        // symbolSize: 120,
+        label: {
+            normal: {
+                show: true,
+                formatter: '{b}',
+                color: '#fff',
+                textStyle: {
+                    fontSize: '20'
+                }
+            },
+
+        },
+
+        itemStyle: {
+            normal: {
+                color: '#00acea'
+            },
+            emphasis: {
+                // symbol: 'image://./img/circle_q_pr.png',
+                color: '#eea631',
+                opacity: 1
+            }
+        },
+        data: datas
+    }];
 option = {
     color: ['rgba(5, 150, 255, 0.7)','rgba(23, 214, 255, 0.7)','rgba(235, 190, 84, 0.7)','rgba(235, 190, 84, 0)'],
-    tooltip: {
-        show: false
-    },
+    // tooltip: {
+    //     show: false
+    // },
     legend: {
         show: false
     },
     toolbox: {
         show: false
     },
+    xAxis: [{
+        gridIndex: 0,
+        type: 'value',
+        show: false,
+        min: 0,
+        max: 100,
+        nameLocation: 'middle',
+        nameGap: 30
+
+
+    }],
+    yAxis: [{
+        gridIndex: 0,
+        min: 0,
+        show: false,
+        max: 100,
+        nameLocation: 'middle',
+        nameGap: 30,
+    }],
     series: seriesObj
 }
 
